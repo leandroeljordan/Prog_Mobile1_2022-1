6ce4d2b0dbaaebdecd26472c3237722c0d4450b9
leandroeljordan <leandroeljordan@gmail.com>
2022-05-26 21:11:42 -0300

    Second Update of first deploy

---
+++ Prog_Mobile_Sem1_2022_EX2/app/src/main/java/com/example/prog_mobile_2_sem_1_2022_ex2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.prog_mobile_2_sem_1_2022_ex2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
